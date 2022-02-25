e5c1c7938479734747ada5649ee9f68f7a961fff
Ximing Chen <steven.xm824@gmail.com>
2022-02-24 16:44:16 -0800

    home page set up

---
+++ components/naviBar.js
@@ -0,0 +1,13 @@
+const NaviBar = () => {
+
+  return (
+    <div className = "navi">
+      <div className = "bar">
+        <span>about</span>
+        <span>contact</span>
+      </div>
+    </div>
+  )
+}
+
+export default NaviBar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-
+import NaviBar from '../components/naviBar.js';
 
 function HomePage() {
 
   return (
   <div className = "homePage">
-    <div className = 'Nav'>navibar goes here</div>
-    <div>Ximing Chen</div>
+    <div className = 'Nav'><NaviBar /></div>
+    <h2 className = 'title'>Ximing Chen</h2>
   </div>)
 }
 
