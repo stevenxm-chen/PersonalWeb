cddb2df08011b2e92665fcc17094b09904f25280
Ximing Chen <steven.xm824@gmail.com>
2022-02-24 16:29:44 -0800

    set up complete

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+
+
+function HomePage() {
+
+  return (
+  <div className = "homePage">
+    <div className = 'Nav'>navibar goes here</div>
+    <div>Ximing Chen</div>
+  </div>)
+}
+
+export default HomePage
