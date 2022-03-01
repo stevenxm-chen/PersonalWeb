24b08c7322b3f5bc11a42e328387f768039e0238
Ximing Chen <steven.xm824@gmail.com>
2022-03-01 11:56:53 -0800

    about

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,17 @@
+import NaviBar from '../components/naviBar.js';
+
 const About = () => {
 
   return (
-    <div>this is about page</div>
+    <div>
+      <div className='Nav'><NaviBar /></div>
+      <div className='subtitle'>
+        <span className='inline'>Hey, I'm</span>
+        <span className='title'>Ximing Chen</span>
+        <span className='inline'>,</span>
+        <span className='inline'>a web developer and a photographer</span>
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,13 @@ import NaviBar from '../components/naviBar.js';
 function HomePage() {
 
   return (
-  <div className = "homePage">
-    <div className = 'Nav'><NaviBar /></div>
-    <div className='subtitle'>
-      <span className='inline'>Hey, I'm</span>
-      <span className = 'title'>Ximing Chen</span>
-      <span className='inline'>,</span>
-      <span className='inline'>a web developer and a photographer</span>
+    <div className="homePage">
+      <div className='Nav'><NaviBar /></div>
+      <div className='subtitle'>
+        <h1 >Hello there</h1>
+        <h1 > Nice to meet you!</h1>
       </div>
-  </div>)
+    </div>)
 }
 
 export default HomePage;
